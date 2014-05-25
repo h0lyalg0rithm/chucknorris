1db786df326e5b3c305d8069c8afd27ee7e4aa27
Suraj Shirvankar <surajshirvankar@gmail.com>
2014-05-25 18:45:36 +0400

    Refactor

--- lib/chucknorris.rb
+++ lib/chucknorris.rb
@@ -6,7 +6,7 @@ class ChuckNorris
     @categories = Set.new
   end
   def self.random
-    response = Unirest.get("http://api.icndb.com/jokes/random")
+    response = Unirest.get "http://api.icndb.com/jokes/random"
     begin
       body = response.body
       if body["type"] == "success"
@@ -17,54 +17,54 @@ class ChuckNorris
     end
   end
   def personal(firstname,lastname)
-    response = Unirest.get(URI.escape("http://api.icndb.com/jokes/random?firstName=#{firstname}&lastName=#{lastname}"))
     begin
-      body = response.body
-      if body["type"] == "success"
-        return body["value"]["joke"]
-      end
-    rescue
-      puts "Error occured"
+      body = request "http://api.icndb.com/jokes/random?firstName=#{firstname}&lastName=#{lastname}"
+      return body["value"]["joke"]
+    rescue ChuckNorris::UnsuccessfulError => e
+      puts e
     end
   end
   def jokes(num=4)
     jokes = []
-    response = Unirest.get(URI.escape("http://api.icndb.com/jokes/random/#{num}"))
     begin
-      body = response.body
-      if body["type"] == "success"
-        body["value"].each do |x|
-          jokes.push x["jokes"]
-        end
+      body = request "http://api.icndb.com/jokes/random/#{num}"
+      body["value"].each do |x|
+        jokes.push x["jokes"]
       end
       return jokes
-    rescue Exception => e
+    rescue ChuckNorris::UnsuccessfulError => e
       puts e
     end
   end
   def joke(num)
-    response = Unirest.get(URI.escape("http://api.icndb.com/jokes/#{num}"))
     begin
-      body = response.body
+      body = request "http://api.icndb.com/jokes/#{num}"
       if body["type"] == "success"
         return body["value"]["joke"]
       end
-    rescue Exception => e
+    rescue ChuckNorris::UnsuccessfulError => e
       puts e
     end
   end
   def get_categories    
-    response = Unirest.get(URI.escape("http://api.icndb.com/categories"))
     begin
-      body = response.body
-      if body["type"] == "success"
-        body["value"].each do |x|
-          @categories << x
-        end
+      body = request "http://api.icndb.com/categories"
+      body["value"].each do |x|
+        @categories << x
       end
       return @categories
-    rescue Exception => e
+    rescue ChuckNorris::UnsuccessfulError => e
       puts e
     end
+  end
+
+  private
+  def request(path)
+    response = Unirest.get(URI.escape(path))
+    body = response.body
+    raise ChuckNorris::UnsuccessfulError if body['value'] == "value"
+    body
+  end
+  class UnsuccessfulError < StandardError
   end
 end
